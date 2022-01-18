c6d6e2d214e8169c8702392705e7171a2c3ae047
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-01-18 11:43:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -320,52 +320,59 @@
         const parseChord = () => {
             while(timeline.length) timeline.pop();
             const secBar = 60 / inputBPM() * 4,
-                  frontChars = new Set('ABCDEFG=_%');
+                  frontChars = new Set('ABCDEFG_=%N'); // N === N.C.
             let idx = 0;
             for(const line of rpgen3.toHan(inputChord()).split('\n').map(v => v.trim())) {
                 if(!line.length || /^#/.test(line)) continue;
-                for(const str of line.split(/[\|→]/)) {
+                for(const str of line.split(/[\|lｌ→]/)) {
                     if(!str.length) continue;
                     const when = idx++ * secBar,
                           a = [];
                     let flag = false;
                     for(let i = 0; i < str.length; i++) {
-                        const char = str[i];
+                        const char = str[i],
+                              prev = str[i - 1];
                         if(!frontChars.has(char)) continue;
-                        else if(str[i - 1] === '/') continue;
+                        else if(prev === '/') continue;
+                        else if(prev === '.' && char === 'C') continue;
                         if(!flag) {
-                            if(char === '=' || char === '_' || char === '%') continue;
+                            if(char === '_' || char === '=' || char === '%') continue;
                             else flag = true;
                         }
                         a.push(i);
                     }
                     if(!a.length) continue;
                     const unitTime = secBar / a.length;
-                    let last = null;
+                    let prev = null;
                     for(const [i, v] of a.entries()) {
-                        const s = str.slice(v, i === a.length - 1 ? str.length : a[i + 1]).replace(/\s+/g,'');
-                        if(s[0] === '=' || s[0] === '_') continue;
+                        const s = str.slice(v, i === a.length - 1 ? str.length : a[i + 1]).replace(/\s+/g,''),
+                              c = s[0];
+                        if(c === '_' || c === 'N') continue;
+                        else if(c === '=') {
+                            prev.duration += unitTime;
+                            continue;
+                        }
                         const _when = when + i * unitTime;
-                        if(s[0] === '%') {
-                            last = {...last};
-                            last.when = _when;
+                        if(c === '%') {
+                            prev = {...prev};
+                            prev.when = _when;
                         }
                         else {
                             const key = s.slice(0, s[1] === '#' ? 2 : 1),
                                   chord = s.slice(key.length).replaceAll(/[\s・]/g, '');
-                            last = {
+                            prev = {
                                 key,
                                 chord,
                                 when: _when,
                                 duration: unitTime
                             };
                         }
-                        timeline.push(last);
+                        timeline.push(prev);
                     }
                 }
             }
-            const last = timeline[timeline.length - 1];
-            endTime = last.when + last.duration;
+            const end = timeline[timeline.length - 1];
+            endTime = end.when + end.duration;
         };
         const stop = () => {
             clearInterval(intervalId);
