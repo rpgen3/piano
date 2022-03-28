e5859cd4e7dca7605cb18d5e8ccea3fdbc8fb112
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-03-28 11:37:46 +0900

    Update sec2delta.mjs

--- mjs/midi/sec2delta.mjs
+++ mjs/midi/sec2delta.mjs
@@ -1,2 +1,2 @@
-export const sec2delta = (sec, bpm = 120, div = 0x01E0) => Math.round(sec * bpm * div / 60);
-export const delta2sec = (delta, bpm = 120, div = 0x01E0) => Math.round(delta / bpm / div * 60);
+export const sec2delta = ({sec, bpm = 120, div = 0x01E0}) => Math.round(sec * bpm * div / 60);
+export const delta2sec = ({delta, bpm = 120, div = 0x01E0}) => Math.round(delta / bpm / div * 60);
