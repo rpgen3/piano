5ea447fd1b8e95a82648aac47bc0b49f32daebe3
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-07-03 00:24:42 +0900

    Create makeMidiTrack.mjs

---
+++ mjs/midi/makeMidiTrack.mjs
@@ -0,0 +1,23 @@
+import {Heap} from 'https://rpgen3.github.io/maze/mjs/heap/Heap.mjs';
+import {fixTrack} from 'https://rpgen3.github.io/piano/mjs/midi/fixTrack.mjs';
+const makeMidiTrack = midiNoteSequence => {
+    const heap = new Heap();
+    for(const {
+        pitch,
+        velocity,
+        start,
+        end
+    } of midiNoteSequence) {
+        for(const [i, v] of [
+            start,
+            end
+        ].entries()) {
+            heap.add(v, {
+                pitch,
+                velocity: i === 0 ? 100 : 0,
+                when: v
+            });
+        }
+    }
+    return fixTrack([...heap]);
+};
