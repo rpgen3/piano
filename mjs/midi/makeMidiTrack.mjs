016d1a5d79cd2595d73161f6213c4259efa087ce
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-07-03 13:05:55 +0900

    Update makeMidiTrack.mjs

--- mjs/midi/makeMidiTrack.mjs
+++ mjs/midi/makeMidiTrack.mjs
@@ -1,23 +1,30 @@
 import {Heap} from 'https://rpgen3.github.io/maze/mjs/heap/Heap.mjs';
 import {fixTrack} from 'https://rpgen3.github.io/piano/mjs/midi/fixTrack.mjs';
-export const makeMidiTrack = midiNoteSequence => {
-    const heap = new Heap();
-    for(const {
-        pitch,
-        velocity,
-        start,
-        end
-    } of midiNoteSequence) {
-        for(const [i, v] of [
+export class MidiTrack {
+    constructor({pitch, velocity, when}) {
+        this.pitch = pitch;
+        this.velocity = velocity;
+        this.when = when;
+    }
+    makeArray(midiNoteArray) {
+        const heap = new Heap();
+        for(const {
+            pitch,
+            velocity,
             start,
             end
-        ].entries()) {
-            heap.add(v, {
-                pitch,
-                velocity: i === 0 ? 100 : 0,
-                when: v
-            });
+        } of midiNoteArray) {
+            for(const [i, v] of [
+                start,
+                end
+            ].entries()) {
+                heap.add(v, {
+                    pitch,
+                    velocity: i === 0 ? 100 : 0,
+                    when: v
+                });
+            }
         }
+        return fixTrack([...heap]);
     }
-    return fixTrack([...heap]);
-};
+}
